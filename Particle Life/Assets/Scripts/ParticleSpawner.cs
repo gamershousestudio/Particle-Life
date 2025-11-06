15f04319ba00f023d5e2c592361255a00e66b5f2
gamershousestudio <gamershousestudio@gmail.com>
2025-11-06 10:46:50 -0500

    Small debug fixes
    Minor issues within ParticleSpawner.cs, issues now resolved.

--- Particle Life/Assets/Scripts/ParticleSpawner.cs
+++ Particle Life/Assets/Scripts/ParticleSpawner.cs
@@ -96,21 +96,21 @@ public class ParticleSpawner : MonoBehaviour
         GameObject white = null;
         GameObject yellow = null;
 
-        if (numberOfAquaParticles > 0) { aqua = new GameObject("Aqua"); aqua.transform.parent = manager.transform; }
-        if (numberOfBlueParticles > 0) { blue = new GameObject("Blue"); blue.transform.parent = manager.transform; }
-        if (numberOfBrownParticles > 0) { brown = new GameObject("Brown"); brown.transform.parent = manager.transform; }
-        if (numberOfDarkGreenParticles > 0) { darkGreen = new GameObject("Dark-Green"); darkGreen.transform.parent = manager.transform; }
-        if (numberOfDarkRedParticles > 0) { darkRed = new GameObject("Dark-Red"); darkRed.transform.parent = manager.transform; }
-        if (numberOfGreenParticles > 0) { green = new GameObject("Green"); green.transform.parent = manager.transform; }
-        if (numberOfMagentaParticles > 0) { magenta = new GameObject("Magenta"); magenta.transform.parent = manager.transform; }
-        if (numberOfNavyBlueParticles > 0) { navyBlue = new GameObject("Navy-Blue"); navyBlue.transform.parent = manager.transform; }
-        if (numberOfOrangeParticles > 0) { orange = new GameObject("Orange"); orange.transform.parent = manager.transform; }
-        if (numberOfPinkParticles > 0) { pink = new GameObject("Pink"); pink.transform.parent = manager.transform; }
-        if (numberOfPurpleParticles > 0) { purple = new GameObject("Purple"); purple.transform.parent = manager.transform; }
-        if (numberOfRedParticles > 0) { red = new GameObject("Red"); red.transform.parent = manager.transform; }
-        if (numberOfTealParticles > 0) { teal = new GameObject("Teal"); teal.transform.parent = manager.transform; }
-        if (numberOfWhiteParticles > 0) { white = new GameObject("White"); white.transform.parent = manager.transform; }
-        if (numberOfYellowParticles > 0) { yellow = new GameObject("Yellow"); yellow.transform.parent = manager.transform; }
+        if (numberOfAquaParticles > 0) aqua = new GameObject("Aqua"); aqua.transform.parent = manager.transform;
+        if (numberOfBlueParticles > 0) blue = new GameObject("Blue"); blue.transform.parent = manager.transform;
+        if (numberOfBrownParticles > 0) brown = new GameObject("Brown"); brown.transform.parent = manager.transform;
+        if (numberOfDarkGreenParticles > 0) darkGreen = new GameObject("Dark-Green"); darkGreen.transform.parent = manager.transform;
+        if (numberOfDarkRedParticles > 0) darkRed = new GameObject("Dark-Red"); darkRed.transform.parent = manager.transform;
+        if (numberOfGreenParticles > 0) green = new GameObject("Green"); green.transform.parent = manager.transform;
+        if (numberOfMagentaParticles > 0) magenta = new GameObject("Magenta"); magenta.transform.parent = manager.transform;
+        if (numberOfNavyBlueParticles > 0) navyBlue = new GameObject("Navy-Blue"); navyBlue.transform.parent = manager.transform;
+        if (numberOfOrangeParticles > 0) orange = new GameObject("Orange"); orange.transform.parent = manager.transform;
+        if (numberOfPinkParticles > 0) pink = new GameObject("Pink"); pink.transform.parent = manager.transform;
+        if (numberOfPurpleParticles > 0) purple = new GameObject("Purple"); purple.transform.parent = manager.transform;
+        if (numberOfRedParticles > 0) red = new GameObject("Red"); red.transform.parent = manager.transform;
+        if (numberOfTealParticles > 0) teal = new GameObject("Teal"); teal.transform.parent = manager.transform; 
+        if (numberOfWhiteParticles > 0) white = new GameObject("White"); white.transform.parent = manager.transform; 
+        if (numberOfYellowParticles > 0) yellow = new GameObject("Yellow"); yellow.transform.parent = manager.transform; 
 
         // Creates every particle
         int c = 0;
@@ -120,6 +120,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(aquaParticle, aqua.transform);
             inst.name = "Aqua Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
             inst.GetComponent<Particle>().index = c;
@@ -136,7 +137,6 @@ public class ParticleSpawner : MonoBehaviour
         for (int i = 0; i < numberOfBlueParticles; i++)
         {
             GameObject inst = Instantiate(blueParticle, blue.transform);
-
             inst.name = "Blue Particle " + (c + 1).ToString();
             inst.layer = 3; // Three is the layer for particles
 
@@ -156,6 +156,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(brownParticle, brown.transform);
             inst.name = "Brown Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -172,6 +173,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(darkGreenParticle, darkGreen.transform);
             inst.name = "Dark-Green Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -188,6 +190,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(darkRedParticle, darkRed.transform);
             inst.name = "Dark-Red Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -204,6 +207,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(greenParticle, green.transform);
             inst.name = "Green Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -220,6 +224,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(magentaParticle, magenta.transform);
             inst.name = "Magenta Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -236,6 +241,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(navyBlueParticle, navyBlue.transform);
             inst.name = "Navy Blue Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -256,6 +262,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(orangeParticle, orange.transform);
             inst.name = "Orange Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -272,6 +279,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(pinkParticle, pink.transform);
             inst.name = "Pink Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -288,6 +296,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(purpleParticle, purple.transform);
             inst.name = "Purple Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -304,6 +313,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(redParticle, red.transform);
             inst.name = "Red Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -320,6 +330,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(tealParticle, teal.transform);
             inst.name = "Teal Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -336,6 +347,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(whiteParticle, white.transform);
             inst.name = "White Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
@@ -352,6 +364,7 @@ public class ParticleSpawner : MonoBehaviour
         {
             GameObject inst = Instantiate(yellowParticle, yellow.transform);
             inst.name = "Yellow Particle " + (c + 1).ToString();
+            inst.layer = 3; // Three is the layer for particles
 
             manager.particles[c] = inst;
 
